619e6f72c568ecce6cfa93ea17e6b0b5f44f7ffe
Benjamin HERSON-MACAREL <benjamin.herson-macarel@kyndryl.com>
2021-09-10 16:58:19 +0200

    Updating content

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3
+            My Fibo App
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
